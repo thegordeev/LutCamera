53093b34fafbc7579cdea77d62f2edbed35098cc
Егор Гордеев <the.gordeev@gmail.com>
2026-01-21 17:36:18 +0300

    Initial Commit

---
+++ LutCamera/LutCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LutCameraApp.swift
+//  LutCamera
+//
+//  Created by Егор Гордеев on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct LutCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
